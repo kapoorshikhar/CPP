2e7383251f4de71cf5f63174743346e8348b92d4
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-15 00:21:37 +0530

    Update Factorial.cpp

--- Learning C++/Function/Factorial.cpp
+++ Learning C++/Function/Factorial.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
-
-int factorial(int num1){
-
+ int factorial(int num1){
+  int ans =1;
+   while (num1>1){
+   ans=ans*num1;
+     num1--;
+   }
+   return ans;
+ }
+int main (){
+  int a ;
+  cout<< "Enter the number to whose factorial is to be calculated : " ;
+  cin >> a;
+  int answer = factorial (a);
+  cout << " The factorial is : "<< answer;
+}
