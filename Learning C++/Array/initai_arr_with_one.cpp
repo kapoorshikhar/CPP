7d20d309a61ef3bbebb12b662f3c7b030d2ec0d5
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-16 11:20:29 +0530

    Update initai_arr_with_one.cpp

--- Learning C++/Array/initai_arr_with_one.cpp
+++ Learning C++/Array/initai_arr_with_one.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std;
+   int main (){
+      int i ;
+      int n;
+      cout << "Enter the size of array : ";
+      cin >> n;
+      int num[n];
+      for (i=0;i<=n;i++){
+         num[i]={1};
+         cout <<" "<< num[i];
+      }
+   }
