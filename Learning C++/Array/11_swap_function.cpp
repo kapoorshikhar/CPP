fcd02bb1a27607c08ff55f13bc677e37d5742f12
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-17 00:22:55 +0530

    Add files via upload

---
+++ Learning C++/Array/11_swap_function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void swapArray(int arr[], int n){
+    for(int i=0; i<n ;i++){
+        if (i+1<n){
+            swap(arr[i], arr[i+1]);
+        }
+        
+    }
+    cout << "The array is swapped : "<< endl;
+}
+void printArray(int arr[],int n){
+    for (int i=0;i<n; i++){
+        cout<< " "<< arr[i];
+    }
+    cout<< endl<<" Array Element bieng printed. "<<endl;
+}   
+
+
+
+int main (){
+     int arr[5]  = {1,2,3,4,5}; 
+     swapArray(arr,5) ;  
+     printArray(arr,5) ;  
+    
+    return 0;
+}
