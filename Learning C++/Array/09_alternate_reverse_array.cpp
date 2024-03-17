ff7735cc1456438b0e9bbdc6777e491c9fe3cdcd
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-17 10:37:54 +0530

    Update 09_alternate_reverse_array.cpp

--- Learning C++/Array/09_alternate_reverse_array.cpp
+++ Learning C++/Array/09_alternate_reverse_array.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
 using namespace std;
-void printarray(int arr[], int n){
-    for ( int i=0; i<n;i++ ){
-        cout<<  arr[i]<< endl;
-    }
 
+void swapArray(int arr[], int n){
+    int r=n;
+    for(int i=0; i<n ;i++){
+             
+        if (i+1<n){
+            swap(arr[i], arr[r-1]);
+            r--;
+            cout<< " "<< r;
+        }
+        
+    }
+    cout << "The array is swapped : "<< endl;
 }
-int main(){}
+void printArray(int arr[],int n){
+    for (int i=0;i<n; i++){
+        cout<< " "<< arr[i];
+    }
+    cout<< endl<<" Array Element bieng printed. "<<endl;
+}   
+
+
+
+int main (){
+     int arr[5]  = {1,2,3,4,5}; 
+     swapArray(arr,5) ;  
+     printArray(arr,5) ;  
+     return 0;
+}
