cca808f05b672733542cfce646e2deaa06c80842
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-18 15:14:21 +0530

    Create 09_greaterNumber.cpp

---
+++ Learning C++/09_greaterNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ int main(){
+int a =10;
+int b=20;
+int c=30;
+if(a>b&&a>c){
+cout<< "A is the greatest number"<< endl;
+}
+elseif (b>c&& b>a){
+cout<< "B is the greatest number "<< endl;
+}
+else{
+cout << "C is the greatest Number"<< endl;
+}
+}
