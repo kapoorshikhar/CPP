38f6c6c9efc6bd9a25bae34853fcfd1160a52acd
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-18 15:10:50 +0530

    Update and rename Even.cpp to OddEven.cpp

---
+++ Learning C++/OddEven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+int i=1;
+
+ cout << "Enter the number here : ";
+
+cin >> n;    
+while (i<=n) {
+    cout <<i<< " ";
+    i=i+1;
+ }
+ return 0;
+}
