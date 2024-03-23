6c3c8f14a5b559b03ba31f311f18952bd5c6a78f
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-23 12:18:49 +0530

    Update SimpleCode.cpp

--- Learning C++/Binary Search/simple code.cpp
+++ Learning C++/Binary Search/simple code.cpp
@@ -1,1 +1,32 @@
-b
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[],int key, int size ){
+    
+    int start=0;    
+    int end =size-1;
+    int mid =(start+end)/2;
+
+    while (start<= end){
+    
+        if (arr[mid]==key){
+            return mid;
+        }
+        else if (key>arr[mid]){
+           start=mid+1;
+        }
+        else  {
+            end=mid-1;
+        }
+        mid =start+(start-end)/2;
+    }
+    return -1;   
+}
+
+int main (){
+    int testArray[6]={1,2,3,4,5,6};
+    int ans =binarySearch(testArray,5,6);
+    cout <<" The element is on the index"<<ans;
+    return 0;
+}
+
