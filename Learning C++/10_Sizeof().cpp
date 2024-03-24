c62c0ed8ba272672814ffc0e4ad5e816f4cc8879
Engr. Shikhar Kapoor <112186527+kapoorshikhar@users.noreply.github.com>
2024-03-24 12:38:15 +0530

    Update 10_Sizeof().cpp

---
+++ Learning C++/10_Sizeof().cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  int x;
+  double y;
+  cout<< " The size of int is :"<< sizeof(x)<<endl;
+  cout<< " The size of int is :"<< sizeof(y)<<endl;
+  cout<< " The size of int is :"<< sizeof(10ll)<<endl;
+return 0;
+}
